398f9ea20cbe15ba7e50e56506e21194b5ceb685
gopi.nutakki@gmail.com <gopi.nutakki@gmail.com>
2012-03-09 15:11:52 -0600

    Testing dataset(same as training) added.

--- spam-filter-x/src/SpamClassifier.java
+++ spam-filter-x/src/SpamClassifier.java
@@ -34,25 +34,41 @@ public class SpamClassifier {
 	static Attribute eClass;
 	static FastVector records;
 	static Instances trainingSet;
+	static Instances testingSet;
+	static Instances evaluationSet;
 
 	public static void main(String args[]) throws Exception {
 		String dataset = "d1";
 		FastVector trainingSet, testingSet;
 		SpamClassifier classifier = new SpamClassifier();
 
-		trainingSet = classifier.createTrainingSet(dataset);
-		testingSet = classifier.createTestingSet(dataset);
+		classifier.createTrainingSet(dataset+"\\training");
+		classifier.createTestingSet(dataset+"\\training");
+		
+		classifier.classifierNaiveBayes();
+		//this.classifierJ48();		
 	}
 
-	private FastVector createTestingSet(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private FastVector createTrainingSet(String dataset) throws Exception {
-
+	private void createTestingSet(String dataset) throws IOException {
 		emailMessage = new Attribute("emailMessage", (FastVector) null);
+		emailClass = new FastVector(3);
+		emailClass.addElement("spam");
+		emailClass.addElement("no spam");
+		emailClass.addElement("?");
+		eClass = new Attribute("emailClass", emailClass);
 
+		records = new FastVector(2);
+		records.addElement(eClass);
+		records.addElement(emailMessage);
+
+		testingSet = new Instances("SpamClsfyTraining", records, 40);
+		testingSet.setClassIndex(0);
+		
+		this.readTestingDataset(dataset);
+	}
+	
+	private void createTrainingSet(String dataset) throws Exception {
+		emailMessage = new Attribute("emailMessage", (FastVector) null);
 		emailClass = new FastVector(3);
 		emailClass.addElement("spam");
 		emailClass.addElement("no spam");
@@ -65,11 +81,8 @@ public class SpamClassifier {
 
 		trainingSet = new Instances("SpamClsfyTraining", records, 40);
 		trainingSet.setClassIndex(0);
-
-		this.readDataset(dataset);
-		this.classifierNaiveBayes();
-		this.classifierJ48();
-		return records;
+		
+		this.readTrainingDataset(dataset);
 	}
 
 	private void classifierNaiveBayes() throws Exception {
@@ -77,14 +90,16 @@ public class SpamClassifier {
 		StringToWordVector stringToVector = new StringToWordVector();
 		stringToVector.setInputFormat(trainingSet);
 		Instances filteredData = Filter.useFilter(trainingSet, stringToVector);
-
+		Instances filteredTestData = Filter.useFilter(testingSet, stringToVector);
+		
 		Classifier cModel = (Classifier) new NaiveBayes();
 		cModel.buildClassifier(filteredData);
 
-		Evaluation eTest = new Evaluation(filteredData);
-		eTest.evaluateModel(cModel, filteredData);
+		Evaluation eTest = new Evaluation(filteredTestData);
+		eTest.evaluateModel(cModel, filteredTestData);
 
 		System.out.println("==== Naive Bayes ===" + eTest.toSummaryString());
+		
 	}
 
 	private void classifierJ48() throws Exception {
@@ -102,7 +117,7 @@ public class SpamClassifier {
 		System.out.println("==== J48 ===" + eTest.toSummaryString());
 	}
 
-	private void readDataset(String dataset) throws IOException {
+	private void readTrainingDataset(String dataset) throws IOException {
 
 		ArrayList<String> fileNames = new ArrayList<String>();
 		ArrayList<String> dataFiles = this.listFiles(dataset, fileNames);
@@ -119,6 +134,23 @@ public class SpamClassifier {
 		}
 	}
 
+	private void readTestingDataset(String dataset) throws IOException {
+
+		ArrayList<String> fileNames = new ArrayList<String>();
+		ArrayList<String> dataFiles = this.listFiles(dataset, fileNames);
+		String data = "";
+		for (int looper = 0; looper < dataFiles.size(); looper++) {
+			data = this.readFileAsString(dataFiles.get(looper));
+			Instance rec = new Instance(2);
+			if (dataFiles.get(looper).contains("nospam")) {
+				rec.setValue((Attribute) records.elementAt(0), "no spam");
+			} else
+				rec.setValue((Attribute) records.elementAt(0), "spam");
+			rec.setValue((Attribute) records.elementAt(1), data);
+			testingSet.add(rec);
+		}
+	}
+	
 	private String readFileAsString(String filePath) throws IOException {
 		byte[] buffer = new byte[(int) new File(filePath).length()];
 		BufferedInputStream f = null;
